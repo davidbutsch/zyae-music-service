3de1ec3dc2501f2593ae2a9859a0fd3670dffc20
David <102764748+davidbzyae@users.noreply.github.com>
2024-02-26 14:09:58 -0600

    feat(session): add session domain object

--- src/modules/session/do/Session.ts
+++ src/modules/session/do/Session.ts
@@ -1,1 +1,12 @@
-export class Session {}
+import { IsDate, IsString } from "class-validator";
+
+import { IsObjectId } from "@/common";
+
+export class Session {
+  @IsObjectId() _id: string;
+  @IsObjectId() userId: string;
+  @IsString() accessToken: string;
+  @IsString() refreshToken: string;
+  @IsDate() expiresAt: string;
+  @IsDate() updatedAt: string;
+}
