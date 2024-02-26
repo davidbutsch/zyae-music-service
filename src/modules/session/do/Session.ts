1887c6e9d02d9541e8c14d2d66cb86ce031f1f81
David <102764748+davidbzyae@users.noreply.github.com>
2024-02-26 14:19:13 -0600

    refactor(session): rename session domain object _id to id

--- src/modules/session/do/Session.ts
+++ src/modules/session/do/Session.ts
@@ -3,7 +3,7 @@ import { IsDate, IsString } from "class-validator";
 import { IsObjectId } from "@/common";
 
 export class Session {
-  @IsObjectId() _id: string;
+  @IsObjectId() id: string;
   @IsObjectId() userId: string;
   @IsString() accessToken: string;
   @IsString() refreshToken: string;
