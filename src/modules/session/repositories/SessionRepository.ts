a9e45f95a94912c717f1515b42b435cacc7ed07f
David <102764748+davidbzyae@users.noreply.github.com>
2024-02-27 13:25:15 -0600

    refactor(session): refactor SessionRepository.findById to use RedisJSON

--- src/modules/session/repositories/SessionRepository.ts
+++ src/modules/session/repositories/SessionRepository.ts
@@ -4,8 +4,13 @@ import { redis } from "@/libs";
 
 export class SessionRepository implements ISessionRepository {
   async findById(id: string): Promise<Session | null> {
-    const raw = await redis.get(`session:${id}`);
-    return raw ? JSON.parse(raw) : null;
+    const key = await redis.call("JSON.GET", `session:${id}`, ".");
+    if (typeof key === "string") {
+      const session: Session = JSON.parse(key);
+      return session;
+    }
+
+    return null;
   }
   findByAccessToken(at: string): Promise<Session | null> {
     throw new Error("Method not implemented.");
