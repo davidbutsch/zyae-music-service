e362e0296d07d63b42026c92d9426dc36434f4b1
David <102764748+davidbzyae@users.noreply.github.com>
2024-03-22 15:59:46 -0500

    fix: validate session id as string

--- src/modules/session/do/Session.ts
+++ src/modules/session/do/Session.ts
@@ -1,7 +1,7 @@
 import { IsDateString, IsMongoId, IsString } from "class-validator";
 
 export class Session {
-  @IsMongoId() id: string;
+  @IsString() id: string;
   @IsMongoId() userId: string;
   @IsString() accessToken: string;
   @IsString() refreshToken: string;
