ef485e430baa60084ec4a9f00afe4147ad70644f
David <102764748+davidbzyae@users.noreply.github.com>
2024-03-22 15:46:00 -0500

    feat(SessionService): remove findById method

--- src/modules/session/services/ISessionService.ts
+++ src/modules/session/services/ISessionService.ts
@@ -1,6 +1,5 @@
 import { Session } from "@/modules/session";
 
 export interface ISessionService {
-  findById(id: string): Promise<Session | null>;
   isSessionExpired(session: Session): boolean;
 }
--- src/modules/session/services/SessionService.ts
+++ src/modules/session/services/SessionService.ts
@@ -8,9 +8,6 @@ export class SessionService implements ISessionService {
     @inject("SessionRepository") private sessionRepository: ISessionRepository
   ) {}
 
-  findById(id: string): Promise<Session | null> {
-    return this.sessionRepository.findById(id);
-  }
   isSessionExpired(session: Session): boolean {
     const currentDate = new Date();
     const expiresAtDate = new Date(session.expiresAt);
