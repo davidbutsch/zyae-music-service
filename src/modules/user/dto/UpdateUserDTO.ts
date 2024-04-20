b5dfcd4d06799d912419aae3f2cba0b65a8a8f15
David <102764748+davidbzyae@users.noreply.github.com>
2024-04-20 18:54:40 -0500

    docs: clarify UpdateUserDTO comments

--- src/modules/user/dto/UpdateUserDTO.ts
+++ src/modules/user/dto/UpdateUserDTO.ts
@@ -5,8 +5,9 @@ class UpdateUserPreferences {}
 
 /**
  * Local users reflect user state in a dedicated user/auth service. Only local properties should be mutable.
+ * ^ if this is a music service, properties pertaining to music data should be mutable.
  *
- * `id`, `profile`, `flags`, and `metadata` are immutable
+ * `id`, `profile`, `flags`, and `metadata` are not local properties and are immutable
  * `preferences` may include local mutable properties defined in a seperate class
  */
 export class UpdateUserDTO {
